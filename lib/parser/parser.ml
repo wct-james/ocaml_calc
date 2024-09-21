cb42c725b642b1a915c39db7f44915a6d10655c8
wct-james <w.james1497@gmail.com>
2024-09-21 13:17:00 +0100

    fix nested brackets

--- lib/parser/parser.ml
+++ lib/parser/parser.ml
@@ -14,54 +14,6 @@ let dump_tokens tokens =
     tokens;
   print_newline ()
 
-let parse_expr tokens =
-  let rec parse_literal tokens =
-    match tokens with
-    | Number n :: rest -> (Literal (Number n), rest)
-    | Minus :: rest ->
-        let right, rest = parse_group rest in
-        (Unary (Minus, right), rest)
-    | LParen :: _ -> parse_group tokens
-    | _ -> failwith "failed on parse_literal"
-  and parse_factor tokens =
-    let left, tokens = parse_literal tokens in
-    match tokens with
-    | Multiply :: rest ->
-        let right, rest = parse_factor rest in
-        (Binary (left, Multiply, right), rest)
-    | Divide :: rest ->
-        let right, rest = parse_factor rest in
-        (Binary (left, Divide, right), rest)
-    | _ -> (left, tokens)
-  and parse_sum tokens =
-    let left, tokens = parse_factor tokens in
-    match tokens with
-    | Add :: rest ->
-        let right, rest = parse_sum rest in
-        (Binary (left, Add, right), rest)
-    | Minus :: rest ->
-        let right, rest = parse_sum rest in
-        (Binary (left, Minus, right), rest)
-    | _ -> (left, tokens)
-  and parse_pow tokens =
-    let left, tokens = parse_sum tokens in
-    match tokens with
-    | Pow :: rest ->
-        let right, rest = parse_pow rest in
-        (Binary (left, Pow, right), rest)
-    | _ -> (left, tokens)
-  and parse_group tokens =
-    match tokens with
-    | LParen :: rest -> (
-        let sub_expr, remaining = parse_group rest in
-        match remaining with
-        | RParen :: rest -> (Group sub_expr, rest)
-        | _ -> failwith "expected right parenthesis")
-    | _ -> parse_pow tokens
-  in
-
-  parse_group tokens
-
 let rec dump_ast ast =
   match ast with
   | Binary (l, t, r) ->
@@ -75,3 +27,46 @@ let rec dump_ast ast =
       print_newline ()
   | Group exp -> dump_ast exp
   | Literal t -> t |> dump_token |> print_string
+
+let parse_expr tokens =
+  let rec parse_literal tokens =
+    match tokens with
+    | Number n :: rest -> (Literal (Number n), rest)
+    | Minus :: rest ->
+        let right, rest = parse_pow rest in
+        (Unary (Minus, right), rest)
+    | LParen :: rest -> (
+        let sub_expr, remaining = parse_pow rest in
+        match remaining with
+        | RParen :: rest -> (Group sub_expr, rest)
+        | _ -> failwith "expected right parenthesis")
+    | _ -> failwith "failed on parse_literal"
+  and parse_factor tokens =
+    let left, tokens = parse_literal tokens in
+    match tokens with
+    | Multiply :: rest ->
+        let right, rest = parse_pow rest in
+        (Binary (left, Multiply, right), rest)
+    | Divide :: rest ->
+        let right, rest = parse_pow rest in
+        (Binary (left, Divide, right), rest)
+    | _ -> (left, tokens)
+  and parse_sum tokens =
+    let left, tokens = parse_factor tokens in
+    match tokens with
+    | Add :: rest ->
+        let right, rest = parse_pow rest in
+        (Binary (left, Add, right), rest)
+    | Minus :: rest ->
+        let right, rest = parse_pow rest in
+        (Binary (left, Minus, right), rest)
+    | _ -> (left, tokens)
+  and parse_pow tokens =
+    let left, tokens = parse_sum tokens in
+    match tokens with
+    | Pow :: rest ->
+        let right, rest = parse_pow rest in
+        (Binary (left, Pow, right), rest)
+    | _ -> (left, tokens)
+  in
+  parse_pow tokens
